a055f951d589f701abec81f54087ee02fe57a934
Alexander Kladov <alexander.a.kladov@gmail.com>
2025-01-25 12:01:06 +0400

    Add total price left

--- findandremove_direct.py
+++ findandremove_direct.py
@@ -20,12 +20,21 @@ def get_data(l):
             tokens[0]=tokens[0][len(quan_token[0]):]
         except:
             quantity = 1
+        price = 0
+        for p_str in tokens[1].split():
+            try:
+                price = int(p_str)
+            except:
+                pass
+            if price>0:
+                break
         key = '('+tokens[0].split('(')[1]+')'
         foil_token = tokens[1].split("FOIL")
         if len(foil_token)>1:
             key += " FOIL"
-        return key, quantity
-    return None, None
+        return key, quantity, price
+    return None, None, None
+
 
 def read_orderfile(fname):
     global order_dict
@@ -46,6 +55,7 @@ def update_topdeck():
     fi = open(inputfile, "r")
     fs = open(resultfile, "w")
     total_cards = 0
+    total_price = 0
     lines = 0
     cards = 0
     lastl = None
@@ -53,8 +63,9 @@ def update_topdeck():
         for l in fi.readlines():
             if l[0] == '-':
                 continue
-            key, quantity = get_data(l)
+            key, quantity, price = get_data(l)
             if key and quantity:
+                
                 if key in order_dict:
                     quan0 = quantity
                     lines += 1
@@ -67,20 +78,19 @@ def update_topdeck():
                         quantity = 0
                     cards += quan0 - quantity
                     if quantity:
-                        total_cards += quantity
                         if l[0] in ['0','1','2','3','4','5','6','7','8','9']:
                             l = str(quantity)+l[len(str(quan0)):]
                     else:
                          l=""
-                else:
-                    total_cards += quantity
+                total_cards += quantity
+                total_price += quantity*price
             if len(l.strip())==0 and lastl==l.strip():
                 l = ""
             lastl = l
             if l:
                 fs.write(l)
         print(f'found lines={lines} cards={cards}')
-        print(f'cards left {total_cards}')
+        print(f'cards left {total_cards} price {total_price}')
         print("not found:")
         print(order_dict)
         fi.close()
